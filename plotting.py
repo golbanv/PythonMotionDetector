83ca1db3d1eab93dfef2457a49fb77ea1d7437a7
golbanv <golbanvladislav@gmail.com>
2020-08-03 18:11:19 +0200

    Fix graph layout

--- plotting.py
+++ plotting.py
@@ -2,7 +2,7 @@ from motion_detector import df
 
 from bokeh.plotting import figure, show, output_file 
 
-p=figure(x_axis_type="datetime", height=100, width=500,  title="Motion Graph")
+p=figure(x_axis_type="datetime", height=100, width=500,  title="Motion Graph", sizing_mode="scale_width")
 
 q=p.quad(left=df["Start"],right=df["End"], bottom=0,top=1, color="green")
 
